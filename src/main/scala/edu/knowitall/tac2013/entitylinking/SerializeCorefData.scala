da15922672c5f7b555f6655ca41cdfd9de344332
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-08-07 09:36:41 -0700

    Removed activate method call from SerializeCorefData

--- src/main/scala/edu/knowitall/tac2013/entitylinking/SerializeCorefData.scala
+++ src/main/scala/edu/knowitall/tac2013/entitylinking/SerializeCorefData.scala
@@ -6,17 +6,8 @@ import java.io.PrintWriter
 import java.io.FileWriter
 
 object SerializeCorefData {
-    var baseDir = "/scratch/"
  
     def main(args: Array[String]) {
-	    val argParser = new OptionParser() {
-	      arg("baseDir", "Path to base directory with entitylinking files.", { s => baseDir = s })
-	    }
-	
-	    if(!argParser.parse(args)) return
-	    
-	    KBPQuery.activate(baseDir)
-	    
 	    val queries = parseKBPQueries(getClass.getResource("tac_2012_kbp_english_evaluation_entity_linking_queries.xml").getPath()).toSeq
 	    val pw = new PrintWriter(new FileWriter("corefmentions.txt"))
 	    for(q <- queries){
