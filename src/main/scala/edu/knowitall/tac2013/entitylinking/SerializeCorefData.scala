cbfb98991d62ebdf3b7b53fbdc2a518b9a6ee13f
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-08-07 09:31:45 -0700

    Changed SerializeCorefData to output to file

--- src/main/scala/edu/knowitall/tac2013/entitylinking/SerializeCorefData.scala
+++ src/main/scala/edu/knowitall/tac2013/entitylinking/SerializeCorefData.scala
@@ -2,6 +2,8 @@ package edu.knowitall.tac2013.entitylinking
 
 import scopt.OptionParser
 import KBPQuery.parseKBPQueries
+import java.io.PrintWriter
+import java.io.FileWriter
 
 object SerializeCorefData {
     var baseDir = "/scratch/"
@@ -16,14 +18,16 @@ object SerializeCorefData {
 	    KBPQuery.activate(baseDir)
 	    
 	    val queries = parseKBPQueries(getClass.getResource("tac_2012_kbp_english_evaluation_entity_linking_queries.xml").getPath()).toSeq
-	    
+	    val pw = new PrintWriter(new FileWriter("corefmentions.txt"))
 	    for(q <- queries){
-	    	  println(q.id)
+	    	  pw.write(q.id)
 	          val corefIntervals = KBPQuery.corefHelper.getCorefMentions(SolrHelper.getRawDoc(q.doc),q.begOffset)
 	          for(cmi <- scala.collection.JavaConversions.asScalaIterable(corefIntervals)){
-	            println(cmi)
+	             pw.write("\t" +cmi)
 	          }
+	    	   pw.write("\n")
 	    }
+	    pw.close()
     }
 
 }
