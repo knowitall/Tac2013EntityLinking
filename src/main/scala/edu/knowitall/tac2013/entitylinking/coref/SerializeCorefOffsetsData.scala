749c6f24d804788eee32d0669d74c4f9a69557cf
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-08-15 16:08:11 -0700

    Path bug again.

--- src/main/scala/edu/knowitall/tac2013/entitylinking/coref/SerializeCorefOffsetsData.scala
+++ src/main/scala/edu/knowitall/tac2013/entitylinking/coref/SerializeCorefOffsetsData.scala
@@ -26,10 +26,10 @@ object SerializeCorefOffsetsData {
     
     def serializeCorefOffsetsData (year: String){
       val queries = parseKBPQueries(getClass.getResource("/edu/knowitall/tac2013/entitylinking/tac_"+year+"_kbp_english_evaluation_entity_linking_queries.xml").getPath()).toSeq
-      val pw = new PrintWriter(new File("./src/main/resources/edu/knowitall/tac2013/enitylinking/coref/"+year+"corefmentions.txt"))
+      val pw = new PrintWriter(new File("./src/main/resources/edu/knowitall/tac2013/entitylinking/coref/"+year+"corefmentions.txt"))
 	    pw.close()
 	    for(q <- queries){
-	       val fw = new FileWriter("./src/main/resources/edu/knowitall/tac2013/enitylinking/coref/"+year+"corefmentions.txt",true)
+	       val fw = new FileWriter("./src/main/resources/edu/knowitall/tac2013/entitylinking/coref/"+year+"corefmentions.txt",true)
 	    	  fw.write(q.id)
 	          val corefIntervals = KBPQuery.corefHelper.getCorefIntervals(SolrHelper.getRawDoc(q.doc),q.begOffset)
 	          for(cmi <- scala.collection.JavaConversions.asScalaIterable(corefIntervals)){
